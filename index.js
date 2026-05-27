2bd67a43a4de67b06a6571966e3c84c4845439ff
Naveen <kolanaveen797@gmail.com>
2026-05-27 19:58:19 +0530

    added

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-require("dotenv").config()
-const mysql=require("mysql2")
-const express=require("express")
-const connection=mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD
-})
+require("dotenv").config();
 
-const app=express()
-app.use(express.json())
+const mysql = require("mysql2");
+const express = require("express");
+const cors = require("cors");
 
-connection.connect((error)=>{
-    if (error) throw error
-    console.log("Database connected")
-})
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.json({
-    message: "Backend Running Successfully",
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+connection.connect((error) => {
+  if (error) throw error;
+
+  console.log("Database connected");
+});
+
+app.get("/databases", (req, res) => {
+  connection.query("SHOW DATABASES", (error, result) => {
+    if (error) {
+      return res.status(500).json({
+        message: "Error while fetching databases",
+      });
+    }
+
+    res.status(200).json(result);
   });
-})
+});
 
-app.get("/databases",(req,res)=>{
-    connection.query("Show databases",(error,result)=>{
-        if (error){
-            return res.status(404).json("Error we are getting")
-        }
-        res.status(200).json(result)
-    })
-})
-
-app.listen(5000,"0.0.0.0",()=>{
-    console.log("server is running")
-})
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
+});
